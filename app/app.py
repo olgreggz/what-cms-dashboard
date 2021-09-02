930f308ee42f3fd54d926c7423d63d6a227f3952
edkrueger <edkrueger@gmail.com>
2021-09-01 21:14:30 -0500

    Add bar charts for competitor market share. Fixes #11.

--- app/app.py
+++ app/app.py
@@ -2,14 +2,24 @@
 import joblib
 import pandas as pd
 import streamlit as st
-from resc.resc import make_sankey  # pylint: disable=import-error
+from resc.resc import make_sankey, make_market_share_bar  # pylint: disable=import-error
 
-df = pd.read_csv("data/plot_data.csv")
-labels = joblib.load("data/labels.joblib")
+sankey_df = pd.read_csv("data/plot_data_sankey.csv")
+sankey_labels = joblib.load("data/labels_sankey.joblib")
 
-count_sankey_fig = make_sankey(df, labels, mode="count")
-revenue_sankey_fig = make_sankey(df, labels, mode="revenue")
+count_sankey_fig = make_sankey(sankey_df, sankey_labels, mode="count")
+revenue_sankey_fig = make_sankey(sankey_df, sankey_labels, mode="revenue")
 
-st.title("CMS Competitor Intel")
+indy_df = pd.read_csv("data/indy_data.csv")
+indy_fig = make_market_share_bar(indy_df, mode="indy")
+
+public_df = pd.read_csv("data/public_data.csv")
+public_fig = make_market_share_bar(public_df, mode="public")
+
+
+st.title("Customer Acquisition and Loss")
 st.plotly_chart(count_sankey_fig)
 st.plotly_chart(revenue_sankey_fig)
+st.title("Market Share Analysis")
+st.plotly_chart(indy_fig)
+st.plotly_chart(public_fig)
--- resc/resc.py
+++ resc/resc.py
@@ -1,6 +1,7 @@
 """Somnething"""
 
 import plotly.graph_objects as go
+import plotly.express as px
 
 
 def make_sankey(df, labels, mode):
@@ -32,4 +33,35 @@ def make_sankey(df, labels, mode):
     )
 
     fig.update_layout(title_text=title, font_size=10)
+    return fig
+
+
+def make_market_share_bar(df, mode):
+    if mode == "indy":
+        title = "Customers by CMS (Independent Schools)"
+        dtick = 50
+    elif mode == "public":
+        title = "Customers by CMS (Public Schools)"
+        dtick = 100
+    else:
+        return ValueError("mode must be one of indy, public")
+
+    df = df.sort_values("cms_2021_08", ascending=False).rename(
+        columns={
+            "cms_2020_07": "Jul 2020",
+            "cms_2020_11": "Nov 2020",
+            "cms_2021_08": "Aug 2021",
+        }
+    )
+    fig = px.bar(
+        df,
+        x="cms",
+        y=["Jul 2020", "Nov 2020", "Aug 2021"],
+        barmode="group",
+        labels={"cms": "CMS",},
+    )
+    fig.update_yaxes(dtick=dtick)
+    fig.update_layout(
+        title_text=title, font_size=10, yaxis_title="Count", legend_title="Date"
+    )
     return fig
